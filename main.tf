7d3fc92d07e4b16201a66acf81ac120b01e1f22c
GerardoBrescia <gerrybrescia99@gmail.com>
2023-09-01 16:30:32 +0200

    MOdified a module in Terraform

--- main.tf
+++ main.tf
@@ -3,11 +3,11 @@ module "network" {
   source = "./network-module"
 
   subnet_cidr       = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-1b"
   vpc_id            = "vpc-0123456789abcdef0"
   allowed_cidr      = "0.0.0.0/0"
 
   # Altre variabili specifiche per il modulo, se necessario
-  router_name       = "example-router"
+  router_name       = "example-router1"
   route_table_name  = "example-route-table"
 }
