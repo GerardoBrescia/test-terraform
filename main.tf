6a1c79b6d2e521751c9acc196337c488514d2cdd
GerardoBrescia <gerrybrescia99@gmail.com>
2023-09-01 16:45:05 +0200

    Fix: ho fixato west

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ module "network" {
   source = "./network-module"
 
   subnet_cidr       = "10.0.1.0/24"
-  availability_zone = "us-east-1b"
+  availability_zone = "us-west-1b"
   vpc_id            = "vpc-0123456789abcdef0"
   allowed_cidr      = "0.0.0.0/0"
 
